bc559f549562535c3a4c7b1fcd0bd34da9a1fa5f
qameta-ci <qameta-ci@qameta.io>
2024-10-03 09:48:36 +0000

    update 2024-10-03T09:48:36

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,9 +1,9 @@
 const { test } = require("@playwright/test");
-const { allure } = require("allure-playwright");
+const allure = require("allure-js-commons");
 
-test("test with links", async() => {
+test("sample test", async () => {
   await allure.links(...[{ url: "https://example.org"}]);
-  await allure.owner("John Doe x");
+  await allure.owner("John Doe");
   await allure.issue("JIRA-2", "https://example.org");
   await allure.step("step 1", async () => {
     await allure.step("step 1.2", async () => {
